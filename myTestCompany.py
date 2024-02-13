69e3efb4dca9f601b4d054b893019ae53bd266f0
Zaid El-Hoiydi <zelidar@gmail.com>
2024-02-13 15:17:52 +0100

    Update myTestCompany.py

--- myTestCompany.py
+++ myTestCompany.py
@@ -1,4 +1,4 @@
 #Identifiers & Credentials for RSign
-ReferenceKey = "QwA1ADAAMgBCADMARABBA"
-EmailId = "zaid.el-hoiydi_tc@frama.com"
-Password = "Cheese.41.Mouse"
+ReferenceKey = "tralala"
+EmailId = "tralala@tralala.com"
+Password = "tralala"
